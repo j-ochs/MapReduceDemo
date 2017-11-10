1fb8566deb55a4ad9b72cd891e1f929394faabb4
Jacob <jochs@westmont.edu>
2017-11-09 16:37:42 -0800

    another commit comoing in hot

--- src/main/java/cs128/cs128/WordCount.java
+++ src/main/java/cs128/cs128/WordCount.java
@@ -1,17 +1,50 @@
 package cs128.cs128;
 
 import java.io.IOException;
+import java.util.Iterator;
+import java.util.StringTokenizer;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapred.FileOutputFormat;
 import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.reduce.IntSumReducer;
 import org.apache.hadoop.util.GenericOptionsParser;
 
 public class WordCount {
+	
+	public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable> {
+		
+		public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+			//read in the data
+			//
+			StringTokenizer itr = new StringTokenizer(value.toString());
+			Text word = new Text();
+			IntWritable one = new IntWritable(1);
+			while (itr.hasMoreTokens()) {
+				word.set(itr.nextToken());
+				context.write(word, one);
+			}
+		}
+		
+	}
+	
+	public static class IntSumReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+		public void reducer(Text term, Iterable<IntWritable> ones, Context context) throws IOException, InterruptedException {
+			int count = 0;
+			Iterator<IntWritable> iterator = ones.iterator();
+			while(iterator.hasNext()) {
+				count++;
+				iterator.next();
+			}
+		}
+		
+	}
 
 	public static void main(String[] args) throws IOException {
 		
