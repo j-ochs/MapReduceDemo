b9a081d1f6cdc23c45ff93a8076151583a796206
Jacob <jochs@westmont.edu>
2017-11-09 16:26:04 -0800

    James is still needy...

--- src/main/java/cs128/cs128/WordCount.java
+++ src/main/java/cs128/cs128/WordCount.java
@@ -1,9 +1,42 @@
 package cs128.cs128;
 
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.mapred.FileOutputFormat;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.reduce.IntSumReducer;
+import org.apache.hadoop.util.GenericOptionsParser;
+
 public class WordCount {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
+		
+		Configuration conf = new Configuration();
+		String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+		
+		if (otherArgs.length == 2) {
+			System.err.println("Usage: WordCount <in> <out>");
+			System.exit(2);
+		}
+		
+		Job job = Job.getInstance(conf, "Word Count");
+		
+		job.setJarByClass(WordCount.class);
+		job.setMapperClass(TokenizerMapper.class);
+		//job.setCombinerClass(IntSumReducer.class);
+		job.setReducerClass(IntSumReducer.class);
+		job.setNumReduceTasks(10);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+		
+		FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
+		FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
+		
 
 	}
 
