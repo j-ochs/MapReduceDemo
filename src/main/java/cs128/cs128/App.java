60462f5d9804b26be56d0af63564a1ebb8205a43
Jacob <jochs@westmont.edu>
2017-11-09 16:20:56 -0800

    initial commit for needy James

---
+++ src/main/java/cs128/cs128/App.java
@@ -0,0 +1,13 @@
+package cs128.cs128;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
